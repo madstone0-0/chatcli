0cb3e7689ab86592ae2f683e84a27329a23f1a8a
Madiba Hudson-Quansah <mhquansah@gmail.com>
2023-11-13 09:06:38 +0000

    feat(models): Added gpt4 turbo preview

--- chatcli/cli.py
+++ chatcli/cli.py
@@ -15,7 +15,7 @@ help_msgs = {
     "tokens": "Max tokens used",
 }
 
-gpt4_models = {"default": "gpt-4", "32k": "gpt-4-32k"}
+gpt4_models = {"default": "gpt-4", "32k": "gpt-4-32k", "turbo": "gpt-4-1106-preview"}
 
 gpt3_5_models = {"default": "gpt-3.5-turbo", "16k": "gpt-3.5-turbo-16k"}
 
@@ -38,6 +38,15 @@ def main(
     )
 ) -> None:
     return
+
+@app.command()
+def ask_gpt4_turbo(
+temp: float = typer.Argument(0.7, help=help_msgs["temp"]),
+    tokens: int = typer.Argument(1000, help=help_msgs["tokens"]),
+    persona: str = typer.Argument("You are a helpful assistant", help="Assistant persona"),
+    is_file: Optional[bool] = typer.Option(None, "--file", help="Read persona from file path"),
+):
+    ask.ask(temp, tokens, model=gpt4_models["turbo"], persona=persona, is_file=is_file)
 
 
 @app.command()
